b27f88ecff0c0b9fa182bba59570c22aa2f8fb57
Andrey Gruzinov <a.gruzinov@pixonic.com>
2018-09-27 15:11:23 +0300

    F! Ability to set event timestamp

---
+++ appmetr/src/main/java/com/appmetr/android/AppmetrConstants.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2018 AppMetr.
+ * All rights reserved.
+ */
+package com.appmetr.android;
+
+public final class AppmetrConstants {
+    public final static String PROPERTY_TIMESTAMP = "timestamp";
+
+    private AppmetrConstants() {
+        throw new UnsupportedOperationException("Only static instance");
+    }
+}
